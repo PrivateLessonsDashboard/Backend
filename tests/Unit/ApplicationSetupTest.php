8865eb81bb6fcf630a1b6090f7fd48aedd9e20be
hajtuJ <bolek.sebastian@gmail.com>
2024-03-01 20:51:07 +0100

    Removed views provider

--- tests/Unit/ApplicationSetupTest.php
+++ tests/Unit/ApplicationSetupTest.php
@@ -6,14 +6,14 @@ use Illuminate\View\ViewServiceProvider;
 use PHPUnit\Framework\TestCase;
 use Tests\CreatesApplication;
 
-class ExampleTest extends TestCase
+class ApplicationSetupTest extends TestCase
 {
 
     use CreatesApplication;
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
+    public function test_view_provider_is_not_loaded(): void
     {
         $app = $this->createApplication();
         $this->assertFalse($app->providerIsLoaded(ViewServiceProvider::class));
