d8b284f1b3b8f758240fbb585dc551012928a6fb
Sebastian Bolek <bolek.sebastian@gmail.com>
2024-03-02 14:49:51 +0100

    feat/PLD-3 - Laravel init, basic cleanup (#2)
    Co-authored-by: Kamil <kamil.krawczak@onet.eu>
    Co-authored-by: Patryk Kuniczak <p.kuniczak@gmail.com>

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\JsonResponse;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * The list of the inputs that are never flashed to the session on validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     */
+    public function register(): void
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+
+    private function getCode(Throwable $e): int
+    {
+        // not all exceptions got method to get http status code, so we need to verify that method exists
+        if (method_exists($e, 'getStatusCode')) {
+            return $e->getStatusCode();
+        }
+        return $e->getCode() ?: Response::HTTP_BAD_REQUEST;
+    }
+
+    public function render($request, Throwable $e)
+    {
+        $code = $this->getCode($e);
+        return new JsonResponse(['error' => ['message' => $e->getMessage(), 'code' => $code]], $code);
+    }
+}
---
+++ tests/Unit/ApplicationSetupTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\View\ViewServiceProvider;
+use PHPUnit\Framework\TestCase;
+use Tests\CreatesApplication;
+
+class ApplicationSetupTest extends TestCase
+{
+
+    use CreatesApplication;
+    /**
+     * A basic test example.
+     */
+    public function test_view_provider_is_not_loaded(): void
+    {
+        $app = $this->createApplication();
+        $this->assertFalse($app->providerIsLoaded(ViewServiceProvider::class));
+    }
+}
