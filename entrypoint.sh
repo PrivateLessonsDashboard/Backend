837e8aceaf0664d945620f5a273db85631034b7c
Patryk Kuniczak <p.kuniczak@gmail.com>
2024-03-03 16:37:25 +0100

    feat(docker-config): create raw config for backend services

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+php artisan key:generate
+php artisan migrate:fresh --seed
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan serve --port="$PORT" --host=0.0.0.0
